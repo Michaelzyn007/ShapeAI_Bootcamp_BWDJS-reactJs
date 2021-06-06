263e6dabfa6fd2ffa738e21d0b23700d1858fae4
Michaelzyn007 <84952789+Michaelzyn007@users.noreply.github.com>
2021-06-06 21:36:59 +0530

    Add files via upload

---
+++ project JS/src/component/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return (
+    <div class="note">
+      <h1>JS & ReactJS</h1>
+      <p>Bootcamp was very exciting and sessions were taught from scratch</p>
+    </div>
+  );
+}
+export default Note;
